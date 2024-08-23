cd021801e88bedc215d0edb8961bc216b3a463a4
mlemko <111366505+mlemko@users.noreply.github.com>
2024-08-23 17:16:28 +1000

    added basic launcher activity

--- app/src/main/java/com/example/linkclipper/FirstFragment.kt
+++ app/src/main/java/com/example/linkclipper/FirstFragment.kt
@@ -1,6 +1,7 @@
 package com.example.linkclipper
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -32,9 +33,9 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+//        binding.buttonFirst.setOnClickListener {
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
     }
 
     override fun onDestroyView() {
