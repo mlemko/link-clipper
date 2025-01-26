7944958fdd02f76b64002c0a7b8b971cbd57860b
mlemko <111366505+mlemko@users.noreply.github.com>
2025-01-27 10:09:30 +1100

    updated build files to newer versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.linkclipper"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.linkclipper"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "0.1"
 
