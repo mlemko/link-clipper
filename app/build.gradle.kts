77c90fc35fc3ce4bd355867f159f7cb0ae993cf2
mlemko <111366505+mlemko@users.noreply.github.com>
2025-01-31 09:24:47 +1100

    update version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.linkclipper"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
